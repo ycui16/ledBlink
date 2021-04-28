31bfa8e94ad0f18bf88988fe72866fbf59efe30e
ycui16 <yc4183@outlook.com>
2021-04-28 22:35:40 +0800

    added led blink code

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,9 +1,17 @@
+int ledPin=13;
+
+
 void setup() {
+  pinMode(ledPin, OUTPUT);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
   // put your main code here, to run repeatedly:
 
 }
