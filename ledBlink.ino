e8941d9df8d0d00b4a2effd5d543c0adeb2948b3
ycui16 <yc4183@outlook.com>
2021-04-28 22:47:25 +0800

    ADD a button

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,20 +1,24 @@
-int ledPin=13;
-int ledPin2=14;
-
+int ledPin = 13;
+int ledPin2 = 14;
+int button1 = 33;
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(button1, INPUT);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  delay(500);
-  digitalWrite(ledPin,LOW);
-  digitalWrite(ledPin2, LOW);
-  delay(500);
-  // put your main code here, to run repeatedly:
+  if (digitalRead(button1) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin2, HIGH);
+    delay(500);
+    digitalWrite(ledPin2, LOW);
+    // put your main code here, to run repeatedly:
 
+  }
 }
